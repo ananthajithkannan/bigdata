eb18e2002a9b267dff387f1a8fdf955e528c3a28
ananthajithkannan <77543477+ananthajithkannan@users.noreply.github.com>
2026-05-18 18:55:10 +0530

    Create STASTICAL_ANALYSIS.r

---
+++ STASTICAL_ANALYSIS.r
@@ -0,0 +1,11 @@
+data <- c(12, 15, 18, 20, 22, 25, 30)
+
+print("Data:")
+print(data)
+
+print(paste("Mean =", mean(data)))
+print(paste("Median =", median(data)))
+print(paste("Standard Deviation =", sd(data)))
+
+print("Summary Statistics:")
+print(summary(data))
