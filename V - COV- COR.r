3868f2716136bd7c5e07ac5c10948fde11364a9e
ananthajithkannan <77543477+ananthajithkannan@users.noreply.github.com>
2026-05-18 18:54:39 +0530

    Create V - COV- COR.r

---
+++ V - COV- COR.r
@@ -0,0 +1,11 @@
+x <- c(2, 4, 6, 8, 10)
+y <- c(4, 8, 12, 16, 20)
+
+print("Variance of x:")
+print(var(x))
+
+print("Covariance of x and y:")
+print(cov(x, y))
+
+print("Correlation of x and y:")
+print(cor(x, y))
